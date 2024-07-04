3d42040b1cdd8bebfe68becb4ef918f36bbeccf8
Nymosdt <dc377303@gmail.com>
2024-07-04 00:21:33 +0000

    new commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-     './src/**/*.astro'
+    './src/pages/**/*.{astro,html,js,jsx,ts,tsx}',
+    './src/components/**/*.{astro,html,js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
